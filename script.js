b89540b2e0fd9de123d3667a8dc069a85a09ce04
Ramon <217152112+OrkByte@users.noreply.github.com>
2025-08-16 12:06:28 +0700

    Add operation functions
    Add basic operation functions for addition, subtraction,
    multiplication, division.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("js linked");
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+
